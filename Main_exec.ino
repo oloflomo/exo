6942452ff918e4aa7a152d448f89126a7137517f
oloflomo <strzalkowski.antoni@gmail.com>
2023-05-25 00:20:46 +0200

    some update

--- Main_exec.ino
+++ Main_exec.ino
@@ -1,3 +1,4 @@
+#include "I2Cdev.h"
 #include "arduinoFFT.h"
 #include <WiFiManager.h>
 #include "RGBHelpers.h"
@@ -7,8 +8,6 @@
 #include <stdio.h>
 #include <freertos/FreeRTOS.h>
 #include <freertos/task.h>
-#include "I2Cdev.h"
-#include "MPU6050.h"
 #include "MPU6050_6Axis_MotionApps20.h"
 
 // Arduino Wire library is required if I2Cdev I2CDEV_ARDUINO_WIRE implementation
@@ -114,11 +113,50 @@ void setup()
   Serial.println(F("Testing device connections..."));
   Serial.println(mpu.testConnection() ? F("MPU6050 connection successful") : F("MPU6050 connection failed"));
 
+  // load and configure the DMP
+  Serial.println(F("Initializing DMP..."));
+  devStatus = mpu.dmpInitialize();
+
   // supply your own gyro offsets here, scaled for min sensitivity
   mpu.setXGyroOffset(220);
   mpu.setYGyroOffset(76);
   mpu.setZGyroOffset(-85);
   mpu.setZAccelOffset(1788); // 1688 factory default for my test chip
+
+
+  // make sure it worked (returns 0 if so)
+    if (devStatus == 0) {
+        // Calibration Time: generate offsets and calibrate our MPU6050
+        mpu.CalibrateAccel(6);
+        mpu.CalibrateGyro(6);
+        mpu.PrintActiveOffsets();
+        // turn on the DMP, now that it's ready
+        Serial.println(F("Enabling DMP..."));
+        mpu.setDMPEnabled(true);
+
+        // enable Arduino interrupt detection
+        //Serial.print(F("Enabling interrupt detection (Arduino external interrupt "));
+        //Serial.print(digitalPinToInterrupt(INTERRUPT_PIN));
+        //Serial.println(F(")..."));
+        //attachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN), dmpDataReady, RISING);
+        mpuIntStatus = mpu.getIntStatus();
+
+        // set our DMP Ready flag so the main loop() function knows it's okay to use it
+        Serial.println(F("DMP ready! Waiting for first interrupt..."));
+        dmpReady = true;
+
+        // get expected DMP packet size for later comparison
+        packetSize = mpu.dmpGetFIFOPacketSize();
+    } else {
+        // ERROR!
+        // 1 = initial memory load failed
+        // 2 = DMP configuration updates failed
+        // (if it's going to break, usually the code will be 1)
+        Serial.print(F("DMP Initialization failed (code "));
+        Serial.print(devStatus);
+        Serial.println(F(")"));
+    }
+
 
   for(int i = 0; i < 128; ++i)
   {
@@ -178,12 +216,11 @@ void loop() //base delay 1000
 {
   delay(1000);
   // read raw accel/gyro measurements from device
-  mpu.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
-
   // these methods (and a few others) are also available
   //accelgyro.getAcceleration(&ax, &ay, &az);
   //accelgyro.getRotation(&gx, &gy, &gz);
-
+  mpu.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
+      // if programming failed, don't try to do anything
   #ifdef OUTPUT_READABLE_ACCELGYRO
       // display tab-separated accel/gyro x/y/z values
       Serial.print(ax); Serial.print(",");
@@ -194,31 +231,34 @@ void loop() //base delay 1000
       Serial.print(gz); Serial.print(",");
   #endif
 
-    #ifdef OUTPUT_BINARY_ACCELGYRO
+  #ifdef OUTPUT_BINARY_ACCELGYRO
         Serial.write((uint8_t)(ax >> 8)); Serial.write((uint8_t)(ax & 0xFF));
         Serial.write((uint8_t)(ay >> 8)); Serial.write((uint8_t)(ay & 0xFF));
         Serial.write((uint8_t)(az >> 8)); Serial.write((uint8_t)(az & 0xFF));
         Serial.write((uint8_t)(gx >> 8)); Serial.write((uint8_t)(gx & 0xFF));
         Serial.write((uint8_t)(gy >> 8)); Serial.write((uint8_t)(gy & 0xFF));
         Serial.write((uint8_t)(gz >> 8)); Serial.write((uint8_t)(gz & 0xFF));
-    #endif
+  #endif
+
+  if (!dmpReady) return;
+    // read a packet from FIFO
   if (mpu.dmpGetCurrentFIFOPacket(fifoBuffer))
   {
     #ifdef OUTPUT_READABLE_WORLDACCEL
-      // display initial world-frame acceleration, adjusted to remove gravity
-      // and rotated based on known orientation from quaternion
-      mpu.GetQuaternion(&q, fifoBuffer);
-      mpu.GetAccel(&aa, fifoBuffer);
-      mpu.GetGravity(&gravity, &q);
-      mpu.GetLinearAccel(&aaReal, &aa, &gravity);
-      mpu.GetLinearAccelInWorld(&aaWorld, &aaReal, &q);
-      //Serial.print("aworld\t");
-      //Serial.print(aaWorld.x);
-      //Serial.print("\t");
-      //Serial.print(aaWorld.y);
-      //Serial.print("\t");
-      //Serial.println(aaWorld.z);
-    #endif
+            // display initial world-frame acceleration, adjusted to remove gravity
+            // and rotated based on known orientation from quaternion
+            mpu.dmpGetQuaternion(&q, fifoBuffer);
+            mpu.dmpGetAccel(&aa, fifoBuffer);
+            mpu.dmpGetGravity(&gravity, &q);
+            mpu.dmpGetLinearAccel(&aaReal, &aa, &gravity);
+            mpu.dmpGetLinearAccelInWorld(&aaWorld, &aaReal, &q);
+            //Serial.print("aworld\t");
+            //Serial.print(aaWorld.x);
+           //Serial.print("\t");
+            //Serial.print(aaWorld.y);
+            //Serial.print("\t");
+            //Serial.println(aaWorld.z);
+      #endif
   }
 
   axtab[iteration] = ax;
